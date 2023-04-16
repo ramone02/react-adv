0c76c017a862acd0d2c818353b8e2b9490679e42
Leonardo Bellido <lbellido@baufest.com>
2023-04-16 13:56:43 -0300

    LazyLoad Basico listo

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,36 +1,56 @@
-import React from "react";
-import { BrowserRouter, Link, NavLink, Navigate, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import {
+  BrowserRouter,
+  Link,
+  NavLink,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import logo from "../assets/react.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages/index";
+import { routes } from "./routes";
+//import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages/index";
 
 //diferencia entre Link y NavLink
 //Link similar <a href=""/> navegacion
 // para estilo si esta en la ruta o no active- not active
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React Logo" />
-          <ul>
-            <li> {/* //isActive valor booleano */}
-              <NavLink to="/lazy1" className={({isActive})=> isActive ? 'nav-active' : ""}>Lazy1</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" className={({isActive})=>isActive ? 'nav-active' : ""}>Lazy2</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" className={({isActive})=>isActive ? 'nav-active' : ""}>Lazy3</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-            <Route path="lazy1" element={<LazyPage1/>}></Route>
-            <Route path="lazy2" element={<LazyPage2/>}></Route>
-            <Route path="lazy3" element={<LazyPage3/>}></Route>
-            <Route path="*" element={<Navigate to="/lazy1" replace/>} ></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Suspense fallback={<span>Loading...</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+              {routes.map((route) => (
+                <li key={route.to}>
+                  {" "}
+                  {/* //isActive valor booleano */}
+                  <NavLink
+                    to={route.to}
+                    className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.to}
+                path={route.path}
+                element={<route.Component />}
+              ></Route> /* route.Component() */
+            ))}
+            <Route
+              path="*"
+              element={<Navigate to={routes[0].to} replace />}
+            ></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
